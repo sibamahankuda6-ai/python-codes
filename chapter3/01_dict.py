6d454d8dda7df838698401be7173451aabbe73fd
Siba Mahankuda <sibamahankuda6@gmail.com>
2026-06-30 21:20:10 +0530

    update

--- chapter3/01_dict.py
+++ chapter3/01_dict.py
@@ -2,5 +2,6 @@ marks={
     "siba":45,
     "hari":83
 }
+
 print(marks, type(marks))
 print(marks["siba"])
--- chapter3/05_sets_Union_intersection.py
+++ chapter3/05_sets_Union_intersection.py
@@ -2,4 +2,6 @@ s1 = {1,45,6}
 s2 = {7,8,1,78}
 
 print(s1.union(s2))
+
+
 print(s1.intersection(s2))
