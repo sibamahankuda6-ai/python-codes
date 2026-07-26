6c650f2358f1dec0c4070f3f25c4f85ba136db7f
Siba Mahankuda <sibamahankuda6@gmail.com>
2026-07-26 22:01:30 +0530

    some changes

--- functions/f3.py
+++ functions/f3.py
@@ -1,6 +1,4 @@
 def argument(name,n):
     print("hello " + name)
     print(n)
-
-
 argument("siba","whats app")
--- functions/f4.py
+++ functions/f4.py
@@ -2,7 +2,5 @@ def argument(name,n):
     print("hello " + name)
     print(n)
     return "oooooo"
-
-
 a = argument("siba","whats app")
 print(a)
